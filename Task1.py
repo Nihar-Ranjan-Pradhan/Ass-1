ec21b61bbdc53928ae01cb575096d01bc46164a5
Nihar-Ranjan-Pradhan <imniharranjan@gmail.com>
2025-09-13 14:48:52 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,13 @@
+#Perform Basic Mathematical operations
+num1 = input('Enter The First Number:')
+num2 = input('Enter The Second Number:')
+num1 = int(num1)
+num2 = int(num2)
+Addition = num1+num2
+Subtraction = num1-num2
+Multiplication = num1*num2
+Division = num1/num2
+print('Addition:',Addition)
+print('Subtraction:',Subtraction)
+print('Multiplication:',Multiplication)
+print('Division:',Division)
---
+++ Task2.py
@@ -0,0 +1,7 @@
+#Create a Personalized greeting.
+str1 = "John"
+str2 = "Doe"
+print('Enter Your First Name:',str1)
+print('Enter Your Last Name:',str2)
+a = str1+str2
+print('Hello,',a +'! Welcome to Python Program.')
